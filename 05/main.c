682b59f5eda7c4692e824e7d2320588d69fa6055
Kirdow <10155603+Kirdow@users.noreply.github.com>
2023-12-06 15:37:14 +0100

    add: day 5

---
+++ 05/main.c
@@ -0,0 +1,267 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 512
+
+typedef struct {
+    uint64_t source;
+    uint64_t dest;
+    uint64_t size;
+} range_t;
+
+typedef struct {
+    range_t* data;
+    uint64_t count;
+} step_t;
+
+typedef struct {
+    step_t* data;
+    uint64_t count;
+} procedure_t;
+
+typedef struct {
+    uint64_t* data;
+    uint64_t count;
+} seeds_t;
+
+static bool is_str_empty(const char* str)
+{
+    const char* ptr = str;
+    while (*ptr != '\0')
+    {
+        char c = *ptr;
+        if (!isspace(c)) return false;
+        ++ptr;
+    }
+
+    return true;
+}
+
+static bool is_map_line(const char* str)
+{
+    const char* ptr = str;
+    while (*ptr != '\0')
+    {
+        if (strncmp(ptr, " map:", 5) == 0) return true;
+        ++ptr;
+    }
+
+    return false;
+}
+
+static bool any_of(char c, const char* list)
+{
+    const char* ptr = list;
+    while (*ptr != '\0')
+    {
+        if (*ptr == c) return true;
+        ++ptr;
+    }
+
+    return false;
+}
+
+static uint64_t get_num_next(const char* ptr, const char** result, const char* until)
+{
+    const char* tmp = ptr;
+    size_t len = 0;
+    while (!any_of(*tmp, until))
+    {
+        ++len;
+        ++tmp;
+    }
+
+    char buffer[len + 1];
+    strncpy(buffer, ptr, sizeof(buffer));
+    char* end;
+    uint64_t num = (uint64_t)strtoll(buffer, &end, 10);
+    *result = tmp;
+    return num;
+}
+
+static bool skip_chars(const char** ptr, const char* list)
+{
+    const char* tmp = *ptr;
+    while (*tmp != '\0' && any_of(*tmp, list)) ++tmp;
+    *ptr = tmp;
+    return !any_of(*tmp, list);
+}
+
+static bool until_chars(const char** ptr, const char* list)
+{
+    const char* tmp = *ptr;
+    while (*tmp != '\0' && !any_of(*tmp, list)) ++tmp;
+    *ptr = tmp;
+    if (*tmp == '\0') return false;
+    return any_of(*tmp, list);
+}
+
+static uint64_t expand(void** data, uint64_t* count, uint64_t size)
+{
+    if (*data == NULL)
+        *data = malloc(size * (*count + 1));
+    else
+        *data = realloc(*data, size * (*count + 1));
+    uint64_t index = *count;
+    ++(*count);
+    return index;
+}
+
+static void add_seed(seeds_t* seeds, uint64_t seed)
+{
+    uint64_t index = expand((void**)&seeds->data, &seeds->count, sizeof(uint64_t));
+    seeds->data[index] = seed;
+}
+
+static void add_range(step_t* step, const range_t* data)
+{
+    uint64_t index = expand((void**)&step->data, &step->count, sizeof(range_t));
+    step->data[index] = *data;
+}
+
+static void add_step(procedure_t* proc, const step_t* data)
+{
+    uint64_t index = expand((void**)&proc->data, &proc->count, sizeof(step_t));
+    proc->data[index] = *data;
+}
+
+static seeds_t get_seeds(const char* line)
+{
+    seeds_t result = {NULL, 0};
+
+    while (*line != '\0')
+    {
+        skip_chars(&line, " ");
+        uint64_t num = get_num_next(line, &line, " ");
+        ++line;
+        uint64_t count = get_num_next(line, &line, " \n");
+        ++line;
+        if (result.data == NULL)
+            result.data = (uint64_t*)malloc(sizeof(uint64_t) * count);
+        else
+            result.data = (uint64_t*)realloc(result.data, sizeof(uint64_t) * (result.count + count));
+        for (uint64_t i = 0; i < count; i++)
+            result.data[result.count + i] = num + i;
+        result.count += count;
+    }
+
+    return result;
+}
+
+static uint64_t run_proc(const procedure_t* proc, uint64_t value)
+{
+    for (uint64_t proc_id = 0; proc_id < proc->count; ++proc_id)
+    {
+        step_t* step = &proc->data[proc_id];
+        for (uint64_t step_id = 0; step_id < step->count; ++step_id)
+        {
+            range_t* range = &step->data[step_id];
+
+            if (value >= range->source && value < range->source + range->size)
+            {
+                value = value + (range->dest - range->source);
+                break;
+            }
+        }
+        
+    }
+
+    return value;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: run <filename.txt>\n");
+        return 1;
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    if (!file)
+    {
+        printf("Failed to open file\n");
+        return 1;
+    }
+
+    seeds_t seeds = {NULL, 0};
+    procedure_t proc = {NULL, 0};
+    step_t step = {NULL, 0};
+
+    char buffer[BUFFER_SIZE];
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        const char* line = buffer;
+        if (strncmp(line, "seeds: ", 7) == 0)
+        {
+            line += 7;
+            if (seeds.data)
+            {
+                printf("Seeds already exist\n");
+                return 1;
+            }
+
+            printf("Fetching seeds\n");
+            seeds = get_seeds(line);
+            continue;
+        }
+
+        if (is_map_line(line))
+        {
+            if (!step.data)
+                continue;
+
+            add_step(&proc, &step);
+            step.data = NULL;
+            step.count = 0;
+            continue;
+        }
+
+        if (is_str_empty(line))
+            continue;
+
+        uint64_t dest = get_num_next(line, &line, " ");
+        ++line;
+        uint64_t src = get_num_next(line, &line, " ");
+        ++line;
+        uint64_t count = get_num_next(line, &line, " \n");
+
+        range_t range = {src, dest, count};
+        add_range(&step, &range);
+    }
+
+    if (step.data)
+        add_step(&proc, &step);
+
+    fclose(file);
+
+    printf("Locating\n");
+    uint64_t result = UINT64_MAX;
+    uint64_t lk = 0;
+    for (uint64_t seed_id = 0; seed_id < seeds.count; ++seed_id)
+    {
+        uint64_t seed = seeds.data[seed_id];
+        uint64_t location = run_proc(&proc, seed);
+        if (location < result) result = location;
+        uint64_t k = seed_id * 10000 / seeds.count;
+        if (lk != k)
+        {
+            uint64_t pc = k / 100;
+            uint64_t pp = k % 100;
+            printf("\r %zu.", pc);
+            if (pp <= 9) printf("0");
+            printf("%zu%%      \r", pp);
+            fflush(stdout);
+        }
+        lk = k;
+    }
+    printf("\r100%%        \n");
+
+    printf("Smallest Location: %zu\n", result);
+
+    return 0;
+}
